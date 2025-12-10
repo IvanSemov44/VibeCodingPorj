152df0f144ca1813213821ca380cbd2bec24cf0c
IvanSemov44 <ivan.semov.44@gmail.com>
2025-12-10 10:29:48 +0200

    chore(db): include ToolSeeder in DatabaseSeeder; remove db_init service

--- tools/db_init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# REMOVED: db_init functionality moved to Laravel DatabaseSeeder.
-echo "This file was removed; seeding is handled via 'php artisan db:seed' and DatabaseSeeder.php"
